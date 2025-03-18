4c3aeac725ea25c03f62768f6dbe13b6cfcc9eee
Muzakkir Hossain Minhaz <minhaz.amj@gmail.com>
2025-03-18 20:14:13 +0600

    Add solution for AtCoder Beginner Contest 152 - A - AC or WA

---
+++ AtCoder Beginner Contest 152/A - AC or WA.c
@@ -0,0 +1,20 @@
+/****************************************************************/
+/*                Author : Muzakkir Hossain Minhaz              */
+/*           Github : github.com/MuzakkirHossainMinhaz          */
+/*      Linkedin : linkedin.com/in/muzakkir-hossain-minhaz      */
+/****************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int N, M;
+    scanf("%d%d", &N, &M);
+
+    if (N == M)
+        printf("Yes\n");
+    else
+        printf("No\n");
+
+    return 0;
+}
