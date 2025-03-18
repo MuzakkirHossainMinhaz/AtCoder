b06091d5c020a735962c2b84610e9c9fc29529a6
Muzakkir Hossain Minhaz <minhaz.amj@gmail.com>
2025-03-18 19:59:13 +0600

    Add solution for AtCoder Beginner Contest 182 - A - twiblr

---
+++ AtCoder Beginner Contest 182/A - twiblr.c
@@ -0,0 +1,16 @@
+/****************************************************************/
+/*                Author : Muzakkir Hossain Minhaz              */
+/*           Github : github.com/MuzakkirHossainMinhaz          */
+/*      Linkedin : linkedin.com/in/muzakkir-hossain-minhaz      */
+/****************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int A, B;
+    scanf("%d%d", &A, &B);
+
+    printf("%d\n", (2 * A + 100) - B);
+    return 0;
+}
